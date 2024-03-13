a429685254dce47096f0f9e00cf4b038746c0f79
Muhammad Maarij <muhammadmaarij7@gmail.com>
2024-03-13 12:20:43 +0500

    api created

--- backend/models/models.py
+++ backend/models/models.py
@@ -9,7 +9,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    notes = relationship("Note", back_populates="owner")
+    # notes = relationship("Note", back_populates="owner")
 
 
 # class Note(Base):
